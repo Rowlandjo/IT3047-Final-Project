85d4dffa3d86fbd9e714bf89a5cf7a8ea72dbb36
Rowlandjo <97526642+Rowlandjo@users.noreply.github.com>
2025-11-02 08:10:05 -0500

    I built the main page with different profiles for each member and the new controllers we need

---
+++ Final_Project_Hobbies/Final_Project_Hobbies/Controllers/GamesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Project_Hobbies.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly HobbyContext _context;
+
+        public GamesController(HobbyContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult List() => View();
+
+        public IActionResult Database()
+        {
+            var games = _context.Games.ToList();
+            return View(games);
+        }
+    }
+}
